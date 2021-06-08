392bf408cf54c5cae7e6a4a59446c343378df874
Matthew Saber <mattsaber21@yahoo.com>
2021-06-07 17:37:40 -0700

    test skeleton

---
+++ red-green.test.js
@@ -0,0 +1,14 @@
+import { getName } from './red-green.js';
+
+describe('test suite', () => {
+  it('gets the name value', () => {
+
+  });
+});
+
+
+const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+getName(spot); // returns spot
+
+const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+getName(character); // returns Aang
